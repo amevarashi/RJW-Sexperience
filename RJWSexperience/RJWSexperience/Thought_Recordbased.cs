a34b5391787cdf0250fb0f1a3be751ee8fdbec7f
amevarashi <kp-karasu@yandex.ru>
2022-04-18 19:11:12 +0500

    Partial revert "Move/copy some classes to Ideology addon"
    This reverts commit 99d3d18d8ac3b1d63483671bbab6c629f687faa1.

--- RJWSexperience/RJWSexperience/Thought_Recordbased.cs
+++ RJWSexperience/RJWSexperience/Thought_Recordbased.cs
@@ -1,40 +1,131 @@
-﻿using RimWorld;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
 
 namespace RJWSexperience
 {
-	/// <summary>
-	/// ThoughtDef using record
-	/// </summary>
-	public class ThoughtDef_Recordbased : ThoughtDef
-	{
-		[SuppressMessage("Minor Code Smell", "S1104:Fields should not have public accessibility", Justification = "Field value loaded from XML")]
-		public RecordDef recordDef;
-		[SuppressMessage("Minor Code Smell", "S1104:Fields should not have public accessibility", Justification = "Field value loaded from XML")]
-		public List<float> minimumValueforStage = new List<float>();
-	}
+    /// <summary>
+    /// ThoughtDef using record
+    /// </summary>
+    public class ThoughtDef_Recordbased : ThoughtDef
+    {
+        public RecordDef recordDef;
+        public List<float> minimumValueforStage = new List<float>();
+        public float increment;
+    }
 
-	/// <summary>
-	/// Thought class using record.
-	/// </summary>
-	public class Thought_Recordbased : Thought_Memory
-	{
-		protected ThoughtDef_Recordbased Def => (ThoughtDef_Recordbased)def;
-		protected RecordDef RecordDef => Def.recordDef;
-		protected List<float> MinimumValueforStage => Def.minimumValueforStage;
+    /// <summary>
+    /// Thought class using record.
+    /// </summary>
+    public class Thought_Recordbased : Thought_Memory
+    {
+        protected ThoughtDef_Recordbased Def => (ThoughtDef_Recordbased)def;
+        protected RecordDef recordDef => Def.recordDef;
+        protected List<float> minimumValueforStage => Def.minimumValueforStage;
+        protected float increment => Def.increment;
 
-		public override int CurStageIndex
-		{
-			get
-			{
-				float value = pawn?.records?.GetValue(RecordDef) ?? 0f;
-				for (int i = MinimumValueforStage.Count - 1; i > 0; i--)
-				{
-					if (MinimumValueforStage[i] < value) return i + 1;
-				}
-				return 0;
-			}
-		}
-	}
+        public override int CurStageIndex
+        {            
+            get
+            {                
+                float value = pawn?.records?.GetValue(recordDef) ?? 0f;
+                for (int i = minimumValueforStage.Count - 1; i > 0; i--)
+                {
+                    if (minimumValueforStage[i] < value) return i + 1;
+                }
+                return 0;
+            }
+        }
+    }
+
+    public class Thought_AteCum : Thought_Recordbased
+    {
+        public override int CurStageIndex
+        {
+            get
+            {
+                if (pawn?.health?.hediffSet?.HasHediff(VariousDefOf.CumAddiction) ?? false) return minimumValueforStage.Count;
+                return base.CurStageIndex;
+            }
+        }
+
+        public override bool TryMergeWithExistingMemory(out bool showBubble)
+        {
+            ThoughtHandler thoughts = pawn.needs.mood.thoughts;
+            if (thoughts.memories.NumMemoriesInGroup(this) >= def.stackLimit)
+            {
+                Thought_AteCum thought_Memory = (Thought_AteCum)thoughts.memories.OldestMemoryInGroup(this);
+                if (thought_Memory != null)
+                {
+                    showBubble = (thought_Memory.age > thought_Memory.def.DurationTicks / 2);
+                    thought_Memory.Merged();
+                    return true;
+                }
+            }
+            showBubble = true;
+            return false;
+        }
+
+        protected virtual void Merged()
+        {
+            age = 0;
+        }
+    }
+
+    public class Thought_IncreaseRecord : Thought_Recordbased
+    {
+        protected float recordIncrement;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref recordIncrement, "recordIncrement", recordIncrement, true);
+        }
+
+        public override void ThoughtInterval()
+        {
+            base.ThoughtInterval();
+            if (recordIncrement != 0)
+            {
+                pawn.records.AddTo(recordDef, recordIncrement);
+                recordIncrement = 0;
+            }
+
+        }
+
+        public override bool TryMergeWithExistingMemory(out bool showBubble)
+        {
+            ThoughtHandler thoughts = pawn.needs.mood.thoughts;
+            if (thoughts.memories.NumMemoriesInGroup(this) >= def.stackLimit)
+            {
+                Thought_IncreaseRecord thought_Memory = (Thought_IncreaseRecord)thoughts.memories.OldestMemoryInGroup(this);
+                if (thought_Memory != null)
+                {
+                    showBubble = (thought_Memory.age > thought_Memory.def.DurationTicks / 2);
+                    thought_Memory.Merged();
+                    return true;
+                }
+            }
+            showBubble = true;
+            return false;
+        }
+
+        public override void Init()
+        {
+            base.Init();
+            recordIncrement = increment;
+        }
+        protected virtual void Merged()
+        {
+            age = 0;
+            recordIncrement += increment;
+        }
+    }
+
+
 }
