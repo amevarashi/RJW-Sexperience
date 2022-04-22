ad76a1857b20c4ea93942c5f0a1c9365ee0d7865
amevarashi <kp-karasu@yandex.ru>
2022-04-22 23:28:59 +0500

    Filters for food with cum

---
+++ RJWSexperience/RJWSexperience/FilterWorkers/SpecialThingFilterWorker_CumBase.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+
+namespace RJWSexperience
+{
+	public abstract class SpecialThingFilterWorker_CumBase : SpecialThingFilterWorker
+	{
+		public override bool CanEverMatch(ThingDef def)
+		{
+			return def.IsIngestible && def.IsProcessedFood;
+		}
+
+		protected bool IsCum(Thing t) => IsCum(t.def);
+
+		protected bool IsCum(ThingDef t) => t == VariousDefOf.GatheredCum;
+
+		protected bool IsFoodWithCum(Thing food)
+		{
+			CompIngredients compIngredients = food.TryGetComp<CompIngredients>();
+
+			if (compIngredients == null)
+				return false;
+
+			foreach (ThingDef ingredient in compIngredients.ingredients)
+			{
+				if (IsCum(ingredient))
+					return true;
+			}
+
+			return false;
+		}
+	}
+}
