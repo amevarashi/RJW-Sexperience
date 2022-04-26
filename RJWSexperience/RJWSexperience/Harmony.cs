78480e6edae12b788de76ceb50a721e23671b0ec
amevarashi <kp-karasu@yandex.ru>
2022-04-26 20:19:11 +0500

    Make cum addicts gravitate to blowjobs

--- RJWSexperience/RJWSexperience/Harmony.cs
+++ RJWSexperience/RJWSexperience/Harmony.cs
@@ -1,6 +1,11 @@
 ﻿using HarmonyLib;
 using System.Reflection;
 using Verse;
+using rjw.Modules.Interactions.Internals.Implementation;
+using rjw.Modules.Interactions.Rules.PartKindUsageRules;
+using System.Collections.Generic;
+using rjw;
+using RJWSexperience.Logs;
 
 namespace RJWSexperience
 {
@@ -11,6 +16,17 @@ namespace RJWSexperience
 		{
 			var har = new Harmony("RJW_Sexperience");
 			har.PatchAll(Assembly.GetExecutingAssembly());
+
+			InjectIntoRjwInteractionServices();
+		}
+
+		public static void InjectIntoRjwInteractionServices()
+		{
+			var log = LogManager.GetLogger("StaticConstructorOnStartup");
+
+			List<IPartPreferenceRule> partKindUsageRules = Unprivater.GetProtectedValue<List<IPartPreferenceRule>>("_partKindUsageRules", typeof(PartPreferenceDetectorService));
+			partKindUsageRules.Add(new Interactions.CumAddictPartKindUsageRule());
+			log.Message("Added 1 rule to PartPreferenceDetectorService._partKindUsageRules");
 		}
 	}
 }
---
+++ RJWSexperience/RJWSexperience/Interactions/CumAddictPartKindUsageRule.cs
@@ -0,0 +1,31 @@
+﻿using rjw.Modules.Interactions.Contexts;
+using rjw.Modules.Interactions.Enums;
+using rjw.Modules.Interactions.Rules.PartKindUsageRules;
+using rjw.Modules.Shared;
+using System.Collections.Generic;
+
+namespace RJWSexperience.Interactions
+{
+	public class CumAddictPartKindUsageRule : IPartPreferenceRule
+	{
+		public IEnumerable<Weighted<LewdablePartKind>> ModifiersForDominant(InteractionContext context)
+		{
+			if (context.Internals.Dominant.Pawn.health?.hediffSet?.HasHediff(VariousDefOf.CumAddiction) ?? false)
+			{
+				// Cum addicts are really eager to use mouth
+				yield return new Weighted<LewdablePartKind>(Multipliers.DoubledPlus, LewdablePartKind.Mouth);
+			}
+		}
+
+		public IEnumerable<Weighted<LewdablePartKind>> ModifiersForSubmissive(InteractionContext context)
+		{
+			Logs.LogManager.GetLogger<CumAddictPartKindUsageRule, Logs.DebugLogProvider>().Warning($"Called for {context.Internals.Submissive.Pawn.NameShortColored}");
+
+			if (context.Internals.Submissive.Pawn.health?.hediffSet?.HasHediff(VariousDefOf.CumAddiction) ?? false)
+			{
+				// Cum addicts are really eager to use mouth
+				yield return new Weighted<LewdablePartKind>(Multipliers.DoubledPlus, LewdablePartKind.Mouth);
+			}
+		}
+	}
+}
