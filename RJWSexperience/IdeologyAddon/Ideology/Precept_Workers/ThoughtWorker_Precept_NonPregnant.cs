a8dc448202fcd5a947b9bc44aa9bcb0674bf56f1
Vegapnk <Vegapnk@protonmail.com>
2022-05-17 22:10:42 +0200

    WIP on Pregnancy Precept

--- RJWSexperience/IdeologyAddon/Ideology/Precept_Workers/ThoughtWorker_Precept_NonPregnant.cs
+++ RJWSexperience/IdeologyAddon/Ideology/Precept_Workers/ThoughtWorker_Precept_NonPregnant.cs
@@ -8,19 +8,19 @@ namespace RJWSexperience.Ideology.Ideology.Precept_Workers
     /// thought worker for a thought that is active when a certain hediff is present, and who's stage depends on the ether state of the pawn 
     /// Shamelessly taken from: https://github.com/Tachyonite/Pawnmorpher/blob/master/Source/Pawnmorphs/Esoteria/Thoughts/ThoughtWorker_EtherHediff.cs
     /// </summary>
-    public class ThoughtWorker_Precept_NonPregnant : ThoughtWorker
+    public class ThoughtWorker_Precept_NonPregnant : ThoughtWorker_Precept
     {
         /// <summary>Gets the current thought state of the given pawn.</summary>
         /// <param name="p">The pawn for whom the thoughts are generated.</param>
         /// <returns></returns>
-        protected override ThoughtState CurrentStateInternal(Pawn p)
+        protected override ThoughtState ShouldHaveThought(Pawn p)
         {
 
             var pregnancy = rjw.PregnancyHelper.GetPregnancy(p);
 
             if (pregnancy == null)
             {
-                return false;
+                return ThoughtState.Inactive;
             }
 
             return ThoughtState.ActiveAtStage(0);
--- RJWSexperience/IdeologyAddon/Ideology/Precept_Workers/ThoughtWorker_Precept_Pregnant.cs
+++ RJWSexperience/IdeologyAddon/Ideology/Precept_Workers/ThoughtWorker_Precept_Pregnant.cs
@@ -9,19 +9,19 @@ namespace RJWSexperience.Ideology.Ideology.Precept_Workers
     /// thought worker for a thought that is active when a certain hediff is present, and who's stage depends on the ether state of the pawn 
     /// Shamelessly taken from: https://github.com/Tachyonite/Pawnmorpher/blob/master/Source/Pawnmorphs/Esoteria/Thoughts/ThoughtWorker_EtherHediff.cs
     /// </summary>
-    public class ThoughtWorker_Precept_Pregnant : ThoughtWorker
+    public class ThoughtWorker_Precept_Pregnant : ThoughtWorker_Precept
     {
         /// <summary>Gets the current thought state of the given pawn.</summary>
         /// <param name="p">The pawn for whom the thoughts are generated.</param>
         /// <returns></returns>
-        protected override ThoughtState CurrentStateInternal(Pawn p)
+        protected override ThoughtState ShouldHaveThought(Pawn p)
         {
             
             var pregnancy = rjw.PregnancyHelper.GetPregnancy(p);
 
-            if (pregnancy == null) //ignoring the hediff's stage, the thought's stage is dependent on only the ether state of the pawn c
+            if (pregnancy == null)
             {
-                return false;
+                return ThoughtState.Inactive;
             }
 
             return ThoughtState.ActiveAtStage(0);
