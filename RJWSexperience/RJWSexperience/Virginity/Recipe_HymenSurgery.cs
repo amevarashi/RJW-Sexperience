7e3034a30095367c3cf589985fbb17005f4be416
amevarashi <kp-karasu@yandex.ru>
2022-06-12 09:21:22 +0500

    Move Recipe_HymenSurgery

---
+++ RJWSexperience/RJWSexperience/Virginity/Recipe_HymenSurgery.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using rjw;
+using System.Collections.Generic;
+using Verse;
+
+namespace RJWSexperience.Virginity
+{
+	public class Recipe_HymenSurgery : Recipe_Surgery
+	{
+		public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
+		{
+			if (pawn.gender != Gender.Female)
+				yield break;
+
+			BodyPartRecord part = Genital_Helper.get_genitalsBPR(pawn);
+			if (part == null)
+				yield break;
+
+			List<Hediff> hediffs = Genital_Helper.get_PartsHediffList(pawn, part);
+			if (Genital_Helper.has_vagina(pawn, hediffs) && !pawn.HasHymen())
+				yield return part;
+		}
+
+		public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+		{
+			if (billDoer == null)
+				return;
+
+			TaleRecorder.RecordTale(TaleDefOf.DidSurgery, new object[]
+			{
+				billDoer,
+				pawn
+			});
+			SurgeryResult(pawn);
+		}
+
+		protected void SurgeryResult(Pawn pawn)
+		{
+			TraitDegree degree = TraitDegree.FemaleAfterSurgery;
+			if (pawn.IsVirgin())
+				degree = TraitDegree.FemaleVirgin;
+			Trait virgin = new Trait(VariousDefOf.Virgin, (int)degree, true);
+			pawn.story.traits.GainTrait(virgin);
+		}
+	}
+}
