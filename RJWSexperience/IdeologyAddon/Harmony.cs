f1af90d499b0320abb24080e505b29e3a0a2d19d
amevarashi <kp-karasu@yandex.ru>
2022-06-18 12:06:29 +0500

    Remove ref to Sexperience from IdeologyAddon

--- RJWSexperience/IdeologyAddon/Harmony.cs
+++ RJWSexperience/IdeologyAddon/Harmony.cs
@@ -1,7 +1,4 @@
-﻿extern alias BaseSexperience;
-using BaseSexperience::RJWSexperience.ExtensionMethods;
-using BaseSexperience::RJWSexperience;
-using HarmonyLib;
+﻿using HarmonyLib;
 using System.Reflection;
 using Verse;
 
@@ -17,11 +14,11 @@ namespace RJWSexperience.Ideology
 
 			if (ModLister.HasActiveModWithName("RJW Sexperience"))
 			{
-				harmony.Patch(AccessTools.Method(typeof(PawnExtensions), nameof(PawnExtensions.IsIncest)),
+				harmony.Patch(AccessTools.Method("RJWSexperience.PawnExtensions:IsIncest"),
 					prefix: new HarmonyMethod(typeof(Sexperience_Patch_IsIncest), nameof(Sexperience_Patch_IsIncest.Prefix)),
 					postfix: null
 					);
-				harmony.Patch(AccessTools.Method(typeof(RJWUtility), nameof(RJWUtility.ThrowVirginHIstoryEvent)),
+				harmony.Patch(AccessTools.Method("RJWSexperience.RJWUtility:ThrowVirginHIstoryEvent"),
 					prefix: null,
 					postfix: new HarmonyMethod(typeof(Sexperience_Patch_ThrowVirginHIstoryEvent), nameof(Sexperience_Patch_ThrowVirginHIstoryEvent.Postfix))
 					);
